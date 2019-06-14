27ef5f86cb03c68f06a13f1066e47c3931e8a28e
Francois Massonnet <francois.massonnet@uclouvain.be>
2019-06-14 10:16:49 +0200

    Python3

--- fig1.py
+++ fig1.py
@@ -8,7 +8,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-exec(open("./namelist.py"))
+if sys.version_info.major == 3:
+    exec(open("./namelist.py").read())
 
 # First set of experiments: standard ITD of LIM
 # ---------------------------------------------    
